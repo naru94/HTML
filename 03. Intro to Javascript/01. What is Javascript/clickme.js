0c53fb1c948bec5fc9c82ed9315f68bb23c0601e
Nimesh Pullarikkal <48006546+naru94@users.noreply.github.com>
2020-08-01 20:57:16 +0530

    Initial commit

---
+++ 03. Intro to Javascript/01. What is Javascript/clickme.js
@@ -0,0 +1,15 @@
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById('button3');
+
+button1.onclick = function() {
+  alert("Button 1 clicked!");
+}
+
+button2.onclick = function() {
+  alert("Button 2 clicked!");
+}
+
+button3.onclick = function() {
+  alert("Button 3 clicked!");
+}
